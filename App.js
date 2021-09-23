8c0c87ff78e92ce8d38696dd9f24931dfd38511a
Mike Estes <sregge@gmail.com>
2021-09-23 01:02:26 -0400

    Added first 2 screens

--- App.js
+++ App.js
@@ -1,21 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+
+import WelcomeScreen from './src/screens/WelcomeScreen';
+import ViewImageScreen from './src/screens/ViewImageScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <ViewImageScreen />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  container: {},
 });
