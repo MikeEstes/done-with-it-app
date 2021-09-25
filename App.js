8967c9999f301e972c6e08e4f2d1b2c339fae137
Mike Estes <sregge@gmail.com>
2021-09-25 02:28:52 -0400

    Implemented Forms with Validation

--- App.js
+++ App.js
@@ -1,42 +1,11 @@
 import React, { useState } from 'react';
 import { StyleSheet, Switch, TextInput, View } from 'react-native';
 
-import MessagesScreen from './src/screens/MessagesScreen';
-import Screen from './src/components/Screen';
-import Icon from './src/components/Icon';
-import ListItem from './src/components/ListItem';
-import AccountScreen from './src/screens/AccountScreen';
-import ListingsScreen from './src/screens/ListingsScreen';
-import AppTextInput from './src/components/AppTextInput';
-import AppPicker from './src/components/AppPicker';
-
-const categories = [
-  { label: 'Furniture', value: 1 },
-  { label: 'Clothing', value: 2 },
-  { label: 'Cameras', value: 3 },
-];
+import ListingEditScreen from './src/screens/ListingEditScreen';
+import LoginScreen from './src/screens/LoginScreen';
 
 export default function App() {
-  const [category, setCategory] = useState(categories[0]);
-
-  return (
-    <Screen>
-      <AppPicker
-        icon='apps'
-        placeholder='Category'
-        items={categories}
-        selectedItem={category}
-        onSelectItem={(item) => setCategory(item)}
-      />
-      <AppTextInput icon='email' placeholder='Email' />
-    </Screen>
-  );
+  return <ListingEditScreen />;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
+const styles = StyleSheet.create({});
