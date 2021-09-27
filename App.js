8ceae84489c01d56a97efde9c98ba229d2d2d3d8
Mike Estes <sregge@gmail.com>
2021-09-26 21:27:04 -0400

    Added local cache, and offline handlers

--- App.js
+++ App.js
@@ -3,11 +3,15 @@ import { NavigationContainer } from '@react-navigation/native';
 
 import AppNavigator from './src/navigation/AppNavigator';
 import navigationTheme from './src/navigation/navigationTheme';
+import OfflineNotice from './src/components/OfflineNotice';
 
 export default function App() {
   return (
-    <NavigationContainer theme={navigationTheme}>
-      <AppNavigator />
-    </NavigationContainer>
+    <>
+      <NavigationContainer theme={navigationTheme}>
+        <AppNavigator />
+      </NavigationContainer>
+      <OfflineNotice />
+    </>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,19 +1,20 @@
 import React from 'react';
-import {
-  View,
-  StyleSheet,
-  Image,
-  TouchableWithoutFeedback,
-} from 'react-native';
+import { StyleSheet, TouchableWithoutFeedback, View } from 'react-native';
+import { Image } from 'react-native-expo-image-cache';
 
 import colors from '../config/colors';
 import Text from './Text';
 
-const Card = ({ imageUrl, onPress, subtitle, title }) => {
+const Card = ({ imageUrl, onPress, subtitle, thumbnailUrl, title }) => {
   return (
     <TouchableWithoutFeedback onPress={onPress}>
       <View style={styles.card}>
-        <Image style={styles.image} source={{ uri: imageUrl }} />
+        <Image
+          style={styles.image}
+          preview={{ uri: thumbnailUrl }}
+          tint='light'
+          uri={imageUrl}
+        />
         <View style={styles.detailsContainer}>
           <Text style={styles.title} numberOfLines={1}>
             {title}
